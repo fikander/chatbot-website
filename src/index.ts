2de14158ae98d3b083e6e94f15c6cd4d051ef4e6
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2022-01-22 16:28:56 +0000

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     // hello world handler
     res.send({"hello": "there"});
 })
 
-app.listen(5000);
+app.listen(5001);
