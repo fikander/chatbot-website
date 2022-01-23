629a2d33ed6c7fd398744c8c52afc1c9a5ee13a0
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2022-01-23 14:28:18 +0000

    Heroku

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -2,9 +2,10 @@ import prodConfig from "./prod";
 import devConfig from "./dev";
 
 type Config = any;
- 
+
 let config: Config
 if (process.env.NODE_ENV === "production") {
+    console.log("production config: " + JSON.stringify(prodConfig));
     config = prodConfig;
 } else {
     config = devConfig;
