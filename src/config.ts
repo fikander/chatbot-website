78562168f4466384da617098362f873a121cd55f
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2022-01-23 14:59:13 +0000

    Heroku

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config = {
     dialogFlowSessionId: process.env.DIALOGLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_SESSION_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 };
 
 export default config;
